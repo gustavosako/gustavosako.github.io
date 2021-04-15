f2c2c5fdbb1dadf6cc855b0f3d8d9bf5678498b0
gustavosako <40046242+gustavosako@users.noreply.github.com>
2021-04-15 04:56:26 -0300

    conexão com a API (cadastro + login)

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ window.addEventListener("load", buttonDialog);
 
 //  MOSTRA/ESCONDE A JANELA DE USUÁRIO
 function buttonDialog() {
-    var button = document.getElementById('loginBtn'),
+    var button = document.getElementById('loginDialog'),
         dialog = document.querySelector('.dialog');
         button.addEventListener("click", function() {
             if (dialog.className === 'dialog show')
@@ -11,3 +11,33 @@ function buttonDialog() {
                 dialog.className = 'dialog show';
     });
 }
+
+// CADASTRO NA API REQ RES
+
+document.getElementById('cadastroBtn').addEventListener('click', function() {
+    var login = document.getElementById('inpLogin'),
+        senha = document.getElementById('inpSenha');
+    axios.post('https://reqres.in/api/register', {email: login.value, password: senha.value})
+        .then((Response) => {
+            console.log(Response);
+        }, (error) => {
+            console.log(error);
+        });
+    login.value = "";
+    senha.value = "";
+});
+
+// LOGIN API REQ RES
+
+document.getElementById('loginBtn').addEventListener('click', function() {
+    var login = document.getElementById('inpLogin'),
+        senha = document.getElementById('inpSenha');
+    axios.post('https://reqres.in/api/login', {email: login.value, password: senha.value})
+        .then((Response) => {
+            console.log(Response);
+        }, (error) => {
+            console.log(error);
+        });
+    login.value = "";
+    senha.value = "";
+});
