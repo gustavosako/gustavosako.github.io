738ea7af718572d1a1157fffbea8079da0fbb4f5
gustavosako <40046242+gustavosako@users.noreply.github.com>
2021-04-15 05:10:42 -0300

    Implementação da verificação dos campos de entrada

--- scripts.js
+++ scripts.js
@@ -16,28 +16,41 @@ function buttonDialog() {
 
 document.getElementById('cadastroBtn').addEventListener('click', function() {
     var login = document.getElementById('inpLogin'),
-        senha = document.getElementById('inpSenha');
-    axios.post('https://reqres.in/api/register', {email: login.value, password: senha.value})
-        .then((Response) => {
-            console.log(Response);
-        }, (error) => {
-            console.log(error);
-        });
-    login.value = "";
-    senha.value = "";
+        senha = document.getElementById('inpSenha'),
+        erro = document.getElementById('error');
+        erro.innerHTML = '';
+    if (login.value.length >= 3 && senha.value.length >= 3) {
+        axios.post('https://reqres.in/api/register', {email: login.value, password: senha.value})
+            .then((Response) => {
+                console.log(Response);
+            }, (error) => {
+                console.log(error);
+            });
+        login.value = "";
+        senha.value = "";
+    } else {
+        erro.innerHTML = 'Os campos devem conter ao menos 3 caracteres.';
+    }
 });
 
 // LOGIN API REQ RES
 
 document.getElementById('loginBtn').addEventListener('click', function() {
     var login = document.getElementById('inpLogin'),
-        senha = document.getElementById('inpSenha');
-    axios.post('https://reqres.in/api/login', {email: login.value, password: senha.value})
-        .then((Response) => {
-            console.log(Response);
-        }, (error) => {
-            console.log(error);
-        });
-    login.value = "";
-    senha.value = "";
+        senha = document.getElementById('inpSenha'),
+        erro = document.getElementById('error');
+        erro.innerHTML = '';
+    if (login.value.length >= 3 && senha.value.length >= 3) {
+        axios.post('https://reqres.in/api/login', {email: login.value, password: senha.value})
+            .then((Response) => {
+                console.log(Response);
+            }, (error) => {
+                console.log(error);
+            });
+        login.value = "";
+        senha.value = "";
+    } else {
+        erro.innerHTML = 'Os campos devem conter ao menos 3 caracteres.';
+    }
+    
 });
